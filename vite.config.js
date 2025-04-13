b829d66a71cfef92e9ca9a47dd2cd8a656a0c227
“JakaPark” <dhxhql3408@naver.com>
2025-04-13 20:45:14 +0900

    vite 수정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/scportfolio/", // 꼭 저장소 이름으로 설정!
   plugins: [react()],
-})
+});
