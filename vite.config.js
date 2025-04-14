e720d5756586c1b1d680916d07bfee2e030b3682
“JakaPark” <dhxhql3408@naver.com>
2025-04-14 15:41:26 +0900

    workflows

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/scportfolio/", // 꼭 저장소 이름으로 설정!
+  base: "/scportfolio/",
   plugins: [react()],
 });
